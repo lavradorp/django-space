300bc66519dc5f3dc6eb9a1017b8ee2ebb4650d9
lavradorp <pedro.lavrador@outlook.com>
2025-01-17 15:00:38 -0300

    projeto base space

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l%37-64*1ehjn@sn!d$4bs97d9y8^jgr7n915b+jd@!%rdan)^'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
